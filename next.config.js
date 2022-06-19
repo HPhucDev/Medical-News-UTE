725a9e73a55340f37d1b4fef6429ce1a75580211
Phuc Nguyen Hoang <hoangphucdeveloper@gmail.com>
2022-06-20 03:45:21 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -22,13 +22,3 @@ module.exports = {
     ];
   },
 };
-export async function getServerSideProps({ req, res }) {
-  res.setHeader(
-    "Cache-Control",
-    "public, s-maxage=10, stale-while-revalidate=59"
-  );
-
-  return {
-    props: {},
-  };
-}

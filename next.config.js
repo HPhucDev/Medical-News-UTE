9b19336d3ff427ee9a9c9b149a9c14708962f3a8
Phuc Nguyen Hoang <hoangphucdeveloper@gmail.com>
2022-06-20 01:08:43 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source:
+          "https://api-ap-northeast-1.graphcms.com/v2/ckxrf0w9z0zgj01z9a5hk59j4/master",
+        destination:
+          "https://api-ap-northeast-1.graphcms.com/v2/ckxrf0w9z0zgj01z9a5hk59j4/master",
+      },
+    ];
+  },
 };
